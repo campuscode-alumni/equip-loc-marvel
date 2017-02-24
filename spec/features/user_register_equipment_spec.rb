6f9e5c2293a84043e94339d40d424f23f27a522c
Charles e Vivi <vwpoit@gmail.com>
2017-02-23 22:09:39 -0300

    [wip] Equipment uses equipment_types

--- spec/features/user_register_equipment_spec.rb
+++ spec/features/user_register_equipment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'User register equipment' do
 
   scenario 'successfully' do
-    #setup
+
     equip = Equipment.new(name: 'Furadeira',
                           description: 'Furadeira Bonita',
                           serial_number: '0001',
@@ -11,11 +11,11 @@ feature 'User register equipment' do
                           acquisition_date: '2017-02-21',
                           shelf_life: '5 anos',
                           picture: 'img/furadeira',
-                          equipment_type: 'alta precisão',
                           manufacture: 'bosch',
                           vendor: 'Zezinho')
 
-    #execução
+    equipment_type = EquipmentType.create(name: 'Betoneiras - Mega Forte 2000')
+
     visit new_equipment_path
 
     fill_in 'Nome',                with: equip.name
@@ -25,13 +25,12 @@ feature 'User register equipment' do
     fill_in 'Data de Aquisição',   with: equip.acquisition_date
     fill_in 'Vida Útil',           with: equip.shelf_life
     fill_in 'Foto',                with: equip.picture
-    fill_in 'Tipo do Equipamento', with: equip.equipment_type
+    select equipment_type.name, from: 'Tipo do Equipamento'
     fill_in 'Fabricante',          with: equip.manufacture
     fill_in 'Fornecedor',          with: equip.vendor
 
     click_on 'Cadastrar Equipamento'
 
-    #expectativa
     expect(page).to have_css('h1', text: equip.name)
     expect(page).to have_content equip.description
     expect(page).to have_content equip.serial_number
@@ -39,19 +38,19 @@ feature 'User register equipment' do
     expect(page).to have_content equip.acquisition_date
     expect(page).to have_content equip.shelf_life
     expect(page).to have_content equip.picture
-    expect(page).to have_content equip.equipment_type
+    expect(page).to have_content equipment_type.name
     expect(page).to have_content equip.manufacture
     expect(page).to have_content equip.vendor
 
   end
 
   scenario 'and should fill all fields' do
-    #execução
-    visit new_equipment_path
 
+    visit new_equipment_path
     click_on 'Cadastrar Equipamento'
-    #expectativa
+
     expect(page).to have_content 'Não foi possível cadastrar o equipamento'
+
   end
 
 end
