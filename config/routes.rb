58c739b8d56d023771ffee03de55bcf385903fe5
Charles e Vivi <vwpoit@gmail.com>
2017-03-09 22:06:11 -0300

    [wip] add sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :budgets, only: [:new, :create]
+  resources :budgets, only: [:new, :create, :index]
   resources :prices, only: [:new, :create, :show]
-  resources :contracts, only: [:new, :create, :show]
-  resources :equipment, only: [:new, :create, :show]
+  resources :contracts, only: [:new, :create, :show, :index]
+  resources :equipment, only: [:new, :create, :show, :index]
   resources :equipment_types, only: [:new, :create, :show]
-  resources :customers, only: [:new, :create, :show]
-
-
+  resources :customers, only: [:new, :create, :show, :index]
 end
