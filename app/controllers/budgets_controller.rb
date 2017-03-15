8ea82fa4101d98c250d002419dc4f84db60d3cee
Charles e Jenifer <vwpoit@gmail.com>
2017-03-14 21:54:03 -0300

    Revisao da view

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,18 +1,22 @@
 class BudgetsController < ApplicationController
 
+  def index
+  end
+
   def new
     @budget = Budget.new
   end
 
   def create
-    budget_params = params.require(:budget).permit(:name, :contact_number, :email, :description)
+    budget_params = params.require(:budget).permit(:name, :contact_number,
+    :email, :description)
     @budget = Budget.new(budget_params)
       if @budget.save
         redirect_to root_path
         flash[:alert] = 'Agradecemos seu interesse em breve entraremos em contato.'
       else
         flash[:error] = 'Preencha os campos em brancos'
-        render :new  
+        render :new
       end
   end
 end
