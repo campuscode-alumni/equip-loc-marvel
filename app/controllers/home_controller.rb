f8c1c2c5b4320f79a3c0ec00b470b7108560b131
Charles e Jenifer <vwpoit@gmail.com>
2017-03-16 22:06:32 -0300

    feat: add near end contracts to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
     if @contracts.empty?
       flash[:empty] = 'Não há nenhum contrato cadastrado'
     end
+    @near_end_contracts = @contracts.where(
+      "end_date <= :two_days", {two_days: Time.zone.today.days_since(2).to_s}
+    ).limit(5)
   end
 
 end
--- spec/features/user_visits_home_spec.rb
+++ spec/features/user_visits_home_spec.rb
@@ -4,8 +4,11 @@ feature 'User visits home' do
 
   scenario 'and finds it pimped up' do
 
-    contract1 = build(:contract)
-    contract2 = build(:contract)
+    contract1 = build(:contract, rental_period: 3)
+    contract1.start_date = Time.zone.today.days_ago(contract1.rental_period - 1).to_s
+    contract2 = build(:contract, rental_period: 10)
+    contract2.start_date = Time.zone.today.days_ago(contract2.rental_period - 2).to_s
+    contract3 = build(:contract, start_date: Time.zone.today.tomorrow.to_s)
 
     customer = create(:customer)
 
@@ -22,17 +25,25 @@ feature 'User visits home' do
     equipment1_contract2 = create(:equipment, equipment_type: equipment_type2)
     equipment2_contract2 = create(:equipment, equipment_type: equipment_type2)
 
+    equipment1_contract3 = create(:equipment, equipment_type: equipment_type1)
+    equipment2_contract3 = create(:equipment, equipment_type: equipment_type2)
+
     contract1.equipment << [equipment1_contract1, equipment2_contract1, equipment3_contract1]
     contract2.equipment << [equipment1_contract2, equipment2_contract2]
+    contract3.equipment << [equipment1_contract3, equipment2_contract3]
 
     contract1.customer = customer
     contract2.customer = customer
+    contract3.customer = customer
 
     contract1.save
     contract2.save
+    contract3.save
 
     visit root_path
 
+    expect(page).to have_css('h2', text: 'Contratos')
+    expect(page).to have_css('h3', text: 'Próximos do término')
     expect(page).to have_content(contract1.id)
     expect(page).to have_content(contract1.customer.name)
     expect(page).to have_content(contract1.total_amount)
@@ -44,6 +55,8 @@ feature 'User visits home' do
     expect(page).to have_content(contract2.total_amount)
     expect(page).to have_content(contract2.start_date)
     expect(page).to have_content(contract2.end_date)
+
+    expect(page).not_to have_content(contract3.end_date)
   end
 
   scenario 'no contracts available' do
